70218928674a844a3f23aa7078f3d34c5536b4c5
Théophane Prevost <theophane.prevost@etu.minesparis.psl.eu>
2026-09-17 11:28:30 +0200

    on ajoute la fonction root

--- fonction1.py
+++ fonction1.py
@@ -1,3 +1,4 @@
+from math import *
 print("3")
 print("4")
 def f3(x):
@@ -6,5 +7,7 @@ def f4(x):
     return(x*4)
 def f5(x):
     return(x*5)
-def root(x):
-    return(x**(1:2))
+def root(x): 
+    return(x**(1:2)) 
+def root2(x):
+    return(square(x))
